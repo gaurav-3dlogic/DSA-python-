02e63fd0ed0e5716df4f0b6d29f64283556e8e5a
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-07-30 16:27:33 +0530

    array

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
-def reveseArray(arr,m):
-    s = m +1 
-    e = len(arr) -1
-    while(s <= e):
-        arr[s],arr[e] = arr[e],arr[s]
-        s += 1
-        e -= 1
-    return arr
+# def reveseArray(arr,m):
+#     s = m +1 
+#     e = len(arr) -1
+#     while(s <= e):
+#         arr[s],arr[e] = arr[e],arr[s]
+#         s += 1
+#         e -= 1
+#     return arr
 
 
-arr = [1,2,3,4,5]
+# arr = [1,2,3,4,5]
 
-print(reveseArray(arr,1))
+# print(reveseArray(arr,1))
+
+def reverseNum(n):
+    if n == 0:
+        return 0
+    else:
+        print(n)
+        reverseNum(n-1)
+n = 5    
+reverseNum(n)
