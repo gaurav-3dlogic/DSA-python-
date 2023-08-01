73834fd131cb114e64712cd62fc608630a57afc0
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-08-01 22:14:32 +0530

    array

--- test.py
+++ test.py
@@ -12,11 +12,29 @@
 
 # print(reveseArray(arr,1))
 
-def reverseNum(n):
-    if n == 0:
-        return 0
-    else:
-        print(n)
-        reverseNum(n-1)
-n = 5    
-reverseNum(n)
+# def reverseNum(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         print(n)
+#         reverseNum(n-1)
+# n = 5    
+# reverseNum(n)
+
+
+
+# python code to Check if element exists in list or not
+
+lst=[ 1, 6, 3, 5, 3, 4 ]
+#checking if element 7 is present
+# in the given list or not
+i=7
+# if element present then return
+# exist otherwise not exist
+if i in lst:
+	print("exist")
+else:
+	print("not exist")
+
+
+	# this code is contributed by gangarajula laxmi
