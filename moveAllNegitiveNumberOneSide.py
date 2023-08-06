a09029ed1002fe424d58ee8eb581dbf5b13e6556
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-08-06 14:29:03 +0530

    array

---
+++ moveAllNegitiveNumberOneSide.py
@@ -0,0 +1,15 @@
+def moveNegOneSide(a):
+    
+    j = 0
+    n = len(a)
+    
+    
+    for i in range(n):
+        if a[i] > 0:
+            a[i] , a[j] = a[j], a[i]
+            j += 1
+    return a 
+
+
+a = [1, -1, 3, 2, -7, -5, 11, 6]
+print(moveNegOneSide(a))        
--- test.py
+++ test.py
@@ -1,22 +1,6 @@
-def sort01(a):
-    # n = len(a)
-    l = 0
-    m = 0
-    h = len(a) -1 
-    
-    
-    while(m <= h):
-        if a[m] == 0:
-            a[m] , a[l] = a[l], a[m]    
-            l += 1
-            m += 1
-        elif a[m] == 1:
-            m += 1
-        else:
-            a[m] , a[h] = a[h] , a[m]
-            h -= 1
-          
-        return a
-a = [2,1,0,1,2]    
-print(sort01(a))    
-        
+# arr1[] = {7, 1, 5, 2, 3, 6} 
+# arr2[] = {3, 8, 6, 20, 7} 
+
+# ans - 
+# {1, 2, 3, 5, 6, 7, 8, 20}
+# {3, 6, 7}
