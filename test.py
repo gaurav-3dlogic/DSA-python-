a97c9e74376fd0c9edcea75d1e9bfda2ce395de8
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-08-08 09:44:44 +0530

    array

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
-a = [1,2,3,2]
-b = [2,3,4,2]
+def kadane(a,n):
+    n = len(a)
+    summ = 0
+    maxSum = 0
+    
+    
+    for i in range(n):
+        summ = a[i]  + summ 
+        
+        maxSum = max(maxSum,summ)
+        
+        
+        if summ < 0:
+            summ = 0
+            
+    return maxSum
 
-c = []
-
-#intersection
-# for i in a:
-#     if i in b and i not in c:
-#         c.append(i)
-# print(c)
-
-#union 
+a = [9,-3,5,0,1,2]
+n = len(a)
+print(kadane(a,n))
