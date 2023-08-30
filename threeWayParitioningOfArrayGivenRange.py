f43c857798561cd1110b6e37995a444b2ea2a2c5
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-08-30 09:48:33 +0530

    array

--- threeWayParitioningOfArrayGivenRange.py
+++ threeWayParitioningOfArrayGivenRange.py
@@ -1,17 +1,13 @@
 def threeWayPartition(arr, n, lowVal, highVal):
  
-    # Initialize ext available positions for
-    # smaller (than range) and greater lements
+
     start = 0
     end = n - 1
     i = 0
  
-    # Traverse elements from left
     while i <= end:
  
-        # If current element is smaller than
-        # range, put it on next available smaller
-        # position.
+
         if arr[i] < lowVal:
             temp = arr[i]
             arr[i] = arr[start]
@@ -19,9 +15,7 @@ def threeWayPartition(arr, n, lowVal, highVal):
             i += 1
             start += 1
  
-        # If current element is greater than
-        # range, put it on next available greater
-        # position.
+
         elif arr[i] > highVal:
             temp = arr[i]
             arr[i] = arr[end]
