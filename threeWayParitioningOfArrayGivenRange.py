51a20ae0946ec4205e5fa8fc234c6ef2ff52512e
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-09-01 09:37:25 +0530

    array

--- threeWayParitioningOfArrayGivenRange.py
+++ threeWayParitioningOfArrayGivenRange.py
@@ -1,35 +1,50 @@
+# Python3 program to implement three way
+# partitioning around a given range.
+
+# Partitions arr[0..n-1] around [lowVal..highVal]
+
+
 def threeWayPartition(arr, n, lowVal, highVal):
- 
 
-    start = 0
-    end = n - 1
-    i = 0
- 
-    while i <= end:
- 
+	# Initialize ext available positions for
+	# smaller (than range) and greater elements
+	start = 0
+	end = n - 1
+	i = 0
 
-        if arr[i] < lowVal:
-            temp = arr[i]
-            arr[i] = arr[start]
-            arr[start] = temp
-            i += 1
-            start += 1
- 
+	# Traverse elements from left
+	while i <= end:
 
-        elif arr[i] > highVal:
-            temp = arr[i]
-            arr[i] = arr[end]
-            arr[end] = temp
-            end -= 1
- 
-        else:
-            i += 1
-    return arr
-            
-arr = [1, 14, 5, 20, 4, 2, 54, 20, 87, 98, 3, 1, 32]  
-lowVal = 14 
-highVal = 20     
-n = len(arr)    
-print(threeWayPartition(arr, n, lowVal, highVal))
+		# If current element is smaller than
+		# range, put it on next available smaller
+		# position.
+		if arr[i] < lowVal:
+			arr[i], arr[start] = arr[start], arr[i]
+			i += 1
+			start += 1
 
-#Output: arr[] = {1, 5, 4, 2, 1, 3, 14, 20, 20, 98, 87, 32, 54}
+		# If current element is greater than
+		# range, put it on next available greater
+		# position.
+		elif arr[i] > highVal:
+			arr[i], arr[end] = arr[end], arr[i]
+			end -= 1
+
+		else:
+			i += 1
+
+# Driver code
+if __name__ == "__main__":
+	arr = [1, 14, 5, 4, 2, 54,
+		20, 87, 98, 3, 1, 32,10]
+	n = len(arr)
+
+	threeWayPartition(arr, n, 10, 20)
+
+	print("Modified array")
+	for i in range(n):
+		print(arr[i], end = " ")
+
+# This code is contributed by
+# sanjeev2552
+        
