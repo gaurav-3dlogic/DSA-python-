d4047bcb47f2d703c6fe0dcbae2780adb0f867f1
gaurav-3dlogic <gs3dlogic@gmal.com>
2023-09-07 10:13:43 +0530

    metrix

--- Metrix/SpiralTraversalOnAMatrix.py
+++ Metrix/SpiralTraversalOnAMatrix.py
@@ -1,8 +1,3 @@
-
-# Python3 program to print
-# given matrix in spiral form
-
-
 def spiralPrint(m, n, a):
 	k = 0
 	l = 0
@@ -14,23 +9,15 @@ def spiralPrint(m, n, a):
 		i - iterator '''
 
 	while (k < m and l < n):
-
-		# Print the first row from
-		# the remaining rows
 		for i in range(l, n):
 			print(a[k][i], end=" ")
 
 		k += 1
-
-		# Print the last column from
-		# the remaining columns
 		for i in range(k, m):
 			print(a[i][n - 1], end=" ")
 
 		n -= 1
 
-		# Print the last row from
-		# the remaining rows
 		if (k < m):
 
 			for i in range(n - 1, (l - 1), -1):
@@ -38,8 +25,6 @@ def spiralPrint(m, n, a):
 
 			m -= 1
 
-		# Print the first column from
-		# the remaining columns
 		if (l < n):
 			for i in range(m - 1, k - 1, -1):
 				print(a[i][l], end=" ")
@@ -47,7 +32,6 @@ def spiralPrint(m, n, a):
 			l += 1
 
 
-# Driver Code
 a = [[1, 2, 3, 4],
 	[5, 6, 7, 8],
 	[9, 10, 11, 12],
@@ -56,10 +40,8 @@ a = [[1, 2, 3, 4],
 R = 4
 C = 4
 
-# Function Call
 spiralPrint(R, C, a)
 
-# This code is contributed by Nikita Tiwari.
 
 
 
