f9a99c35e9f29183752857895a53b7fdbb537af0
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-10-10 09:52:57 +0530

    test

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-n = "Gaurav"
-print(n)
+a = [4,1,3,2]
+
+m = a[0]
+
+
+for i in a:
+    if i >= m:
+        m = i
+print("The max number is",m)
+
+
+for i in a:
+    if i <= m:
+        m = i
+print("The min number is",m)
