a64b102fc01f66d0f6eb862aeaacadf39ad4c345
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-10-11 12:46:41 +0530

    array

--- test.py
+++ test.py
@@ -1,18 +1,15 @@
-def sort01(arr,n):
-    l,m,h = 0,0,len(arr) -1
-    while(m <= h):
-        if(arr[m] == 0):
-            arr[m] , arr[l] = arr[l] , arr[m]
-            m += 1
-            l += 1
-        elif(arr[m] == 1):
-            m  += 1
-        else:
-            arr[m] , arr[h] = arr[h] , arr[m]
-            h -= 1
+#Move all negitive numbers one side
 
-    return arr
-arr = [2,1,0,2,1,0,1,0]
+def moveAllnegitive(arr,n):
+    j = 0
+    for i in range(0,n):
+        if arr[i] > 0:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            j += 1
+    print(arr)
+
+arr = [1,-10,9,1,-7,1,-23]
 n = len(arr)
-print(sort01(arr,n))
-    
+print(moveAllnegitive(arr,n))
