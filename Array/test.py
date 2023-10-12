d2a767b3d78c8ff4e6b9f6df3f7691dd1d033f11
yash <dev04@cyno.co.in>
2023-10-12 16:50:54 +0530

    bug fixes

--- Array/test.py
+++ Array/test.py
@@ -1,23 +1,5 @@
-def findMinDiff(arr, n, m):
-
-
-	if (m == 0 or n == 0):
-		return 0
-
-	arr.sort()
-
-	if (n < m):
-		return -1
-
-	min_diff = arr[n-1] - arr[0]
-	for i in range(len(arr) - m + 1):
-		min_diff = min(min_diff, arr[i + m - 1] - arr[i])
-
-	return min_diff
-
-
-arr = [7, 3, 2, 4, 9, 12, 56]
-m = 3
-n = len(arr)
-print("Minimum difference is", findMinDiff(arr, n, m))
-
+n = int(input("Enter the Number: "))
+f = 1
+for i in range(1,n+1):
+    f = i * f
+print(f)
