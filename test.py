ab26236678fee0c39c43f8611085eac2c922c3f1
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-10-12 10:29:18 +0530

    array

--- test.py
+++ test.py
@@ -1,34 +1,22 @@
-def find_union_and_intersection(arr1, arr2):
-    union_result = []
-    intersection_result = []
-    i, j = 0, 0
+# Interesection and union of two sorted array 
+
+
+def findUnionAndIntersection(arr1,arr2):
+    union =  []
+    intersection = []
+    i,j = 0,0
 
     while i < len(arr1) and j < len(arr2):
         if arr1[i] < arr2[j]:
-            union_result.append(arr1[i])
+            union.append(arr1[i])
             i += 1
         elif arr1[i] > arr2[j]:
-            union_result.append(arr2[j])
+            union.append(arr1[j])
             j += 1
         else:
-            union_result.append(arr1[i])
-            intersection_result.append(arr1[i])
+            union.append(arr1[i])
+            intersection.append(arr1[i])
             i += 1
             j += 1
+        
 
-    # Add remaining elements from both arrays to the union_result
-    while i < len(arr1):
-        union_result.append(arr1[i])
-        i += 1
-    while j < len(arr2):
-        union_result.append(arr2[j])
-        j += 1
-
-    return union_result, intersection_result
-
-# Example usage
-arr1 = [1, 3, 4, 5, 7]
-arr2 = [2, 3, 5, 6]
-union_result, intersection_result = find_union_and_intersection(arr1, arr2)
-print("Union:", union_result)
-print("Intersection:", intersection_result)
