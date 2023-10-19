833c9937bb6a4a0c5bdf2d5d391a8a3849b31442
yash <dev04@cyno.co.in>
2023-10-19 23:05:50 +0530

    array

--- Array/ArrayOfSizeNAndNumberKFindAllElementsThatAppearMoreThan_n/demo.py
+++ Array/ArrayOfSizeNAndNumberKFindAllElementsThatAppearMoreThan_n/demo.py
@@ -1,3 +1,13 @@
 n = int(input("Enter a number: "))
 
-print(n)
+if(n >= 2):
+    for i in range(2, n):
+        if(n % i == 0):
+            print("Number is not prime")
+            break
+            
+        else:
+            print("Number is prime")
+            
+else:
+    print("Number is not prime")
