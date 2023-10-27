c6bf6e7d26c3e4fd0651515d56997ffaa0c8de03
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-10-27 10:57:39 +0530

    demo

--- test.py
+++ test.py
@@ -1,22 +1,3 @@
-# Interesection and union of two sorted array 
+import random
 
-
-def findUnionAndIntersection(arr1,arr2):
-    union =  []
-    intersection = []
-    i,j = 0,0
-
-    while i < len(arr1) and j < len(arr2):
-        if arr1[i] < arr2[j]:
-            union.append(arr1[i])
-            i += 1
-        elif arr1[i] > arr2[j]:
-            union.append(arr1[j])
-            j += 1
-        else:
-            union.append(arr1[i])
-            intersection.append(arr1[i])
-            i += 1
-            j += 1
-        
-
+print(random.randrange(0,5))
