d17c9b4818ba9468f52614c922a382fb787a4c83
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-11-01 09:54:38 +0530

    demo

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
-#Revers a string
 
-n = "abc"
-r = ""
-for i in n:
-    r = i + r
+def reversString(b):
+    str = list(b)
+    n = len(str)
+    s = 0
+    e = n - 1
 
-print(r)
+
+    while s < e:
+        str[s] , str[e] = str[e] , str[s]
+        s += 1
+        e -= 1
+        rev = ''.join(str)
+        return rev
+
+str = "abc"
+reversed_str = reversString(str)
+print(reversed_str)
+
 
