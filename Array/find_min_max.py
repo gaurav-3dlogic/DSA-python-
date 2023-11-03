47d792016712751024d2dd31ec97ba15ca0179f1
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-11-03 10:06:44 +0530

    demo

--- Array/find_min_max.py
+++ Array/find_min_max.py
@@ -2,3 +2,23 @@ a = [1,5,2,3,4,5]
 a.sort()
 
 print(a[0],a[-1])
+
+
+
+a = [1,2,4,5,10,3,6,90,-9,-2,0]
+# after sort -- [-9, -2, 0, 1, 2, 3, 4, 5, 6, 10, 90]
+
+for i in range(len(a)):
+    m = a[0]
+    for j in a:
+        if j >= m:
+            m = j 
+
+
+for i in range(len(a)):
+    n = a[0]
+    for j in a:
+        if j <= n:
+            n = j 
+print("The max number is",m)
+print("The min number is",n)
--- test.py
+++ test.py
@@ -1,13 +1,20 @@
+
+
+
+
+def max_min(a):
+    max_val = float("-Inf")
+    min_val = float("-Inf")
+    for key,values in a.items():
+        if values < min_val:
+            min_val = values
+        if values > max_val:
+            max_val = values
+    return max_val, min_val
+
+
 a = {"a": 1, "b": 2, "c": 3}
-dict_a = {}
+   
+print(max_min(a))
 
-# while a:
-#     key,values = a.popitem()
-#     dict_a[key] = values
-# print(str(dict_a))
-
-
-for key,values in a.items():
-    dict_a[values] = key
-print(dict_a)
 
