e7d37f1781081fd806a828ffb073fa48d30aa858
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-11-06 10:07:45 +0530

    demo

--- test2.py
+++ test2.py
@@ -1,49 +1,13 @@
-def spiralOrder(matrix):
-    row = len(matrix)
-    col = len(matrix[0])
-    count = 0
-    total = row * col
-    ans = []
+def max_min(a):
+    max_val = float("-Inf")
+    min_val = float("Inf")
+    for key,values in a.items():
+        if values > max_val:
+            max_val = values
+        if values < min_val:
+            min_val = values
 
-    # Index initialization
-    startingRow = 0
-    startingCol = 0
-    endingRow = row - 1
-    endingCol = col - 1
+    return max_val,min_val
 
-    while count < total:
-        # Print starting row
-        for i in range(startingCol, endingCol + 1):  # Changed 'and' to '+'
-            ans.append(matrix[startingRow][i])  # Fixed the indexing
-            count += 1
-
-        startingRow += 1
-
-        # Print ending column
-        for i in range(startingRow, endingRow + 1):  # Changed 'and' to '+'
-            ans.append(matrix[i][endingCol])
-            count += 1
-        endingCol -= 1
-
-        # Print ending row
-        if startingRow <= endingRow:  # Check if startingRow is still valid
-            for i in range(endingCol, startingCol - 1, -1):  # Changed 'and' to '-1' and added startingCol - 1
-                ans.append(matrix[endingRow][i])
-                count += 1
-            endingRow -= 1
-
-        # Print starting column
-        if startingCol <= endingCol:  # Check if startingCol is still valid
-            for i in range(endingRow, startingRow - 1, -1):  # Changed 'and' to '-1' and added startingRow - 1
-                ans.append(matrix[i][startingCol])
-                count += 1
-            startingCol += 1
-
-    return ans
-
-matrix = [[1, 2, 3, 4],
-          [5, 6, 7, 8],
-          [9, 10, 11, 12],
-          [13, 14, 15, 16]]
-
-print(spiralOrder(matrix))
+a = {"a":1,"b":2,"c":3,"d":4,"e":5}
+print(max_min(a))
