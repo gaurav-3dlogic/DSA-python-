89cfdbcaec71eb091ac57731cffacd2e9c4e5435
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-11-07 23:37:53 +0530

    demo

--- test2.py
+++ test2.py
@@ -1,48 +1,20 @@
-# def max_min(a):
-#     max_val = float("-Inf")
-#     min_val = float("Inf")
-#     for key,values in a.items():
-#         if values > max_val:
-#             max_val = values
-#         if values < min_val:
-#             min_val = values
+def sort0_1(arr,n):
+    l,m,h = 0,0, len(arr) -1
 
-#     return max_val,min_val
+    while m <= h:
+        if arr[m] == 0:
+            arr[m], arr[l] = arr[l] , arr[m]
+            l += 1
+            m += 1
+        elif arr[m] == 1:
+            m += 1
 
-# a = {"a":1,"b":2,"c":3,"d":4,"e":5}
-# print(max_min(a))
+        else:
+            arr[m], arr[h] = arr[h] , arr[m]
+            h -= 1
+    return arr
+arr = [2,0,1,1,2,0,1,2,0]
+n = len(arr)
+print(sort0_1(arr,n))
 
 
-#Reverse dictionary in python 
-# dic = {"a":1,"b":2,"c":3,"d":4}
-# rev_dict = {}
-# while dic:
-#     key,value = dic.popitem()
-#     rev_dict[key] = value
-
-# print(str(rev_dict))
-    
-
-# revert key with value
-# def reverse_dict(input_dict):
-#     reversed_dict = {}  # Initialize an empty dictionary to store reversed key-value pairs
-
-#     for key, value in input_dict.items():
-#         reversed_dict[value] = key  # Swap key and value in the reversed dictionary
-
-#     return reversed_dict
-
-# # Example usage:
-# input_dict = {"a": 1, "b": 2, "c": 3}
-# reversed_dict = reverse_dict(input_dict)
-# print(reversed_dict)
-
-#Reverse key with values
-def reverse_dict(a):
-    rev_dict = {}
-    for key,value in a.items():
-        rev_dict[value] = key
-    return rev_dict
-
-a = {"a":1, "b":2, "c":3, "d":4}
-print(reverse_dict(a))
