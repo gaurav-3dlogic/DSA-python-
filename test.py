51182c0daa17967957ec3282444b02166bc70bc7
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-11-08 16:52:57 +0530

    demo

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
+
+
+def moveneg(a):
+
+    j = 0
+    
+    for i in range(len(a)):
+        if a[i] < 0:
+            a[i] , a[j] = a[j], a[i]
+            j += 1
+        
+    return a
+
 a = [7,6,5,0,8,-5,6,-3,2]
-a.sort()
-print(a)
 
-k = 4
-
-print(a[k-1])
-print(a[-k])
+print(moveneg(a))
