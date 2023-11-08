0a8e7a0c7235f3ac88c88d666c743bc68cac8473
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-11-08 16:47:48 +0530

    demo

--- test.py
+++ test.py
@@ -1,21 +1,8 @@
-def sort0_1(a,n):
+a = [7,6,5,0,8,-5,6,-3,2]
+a.sort()
+print(a)
 
-    n = len(a)
-    l , m , h = 0 , 0 , n - 1
-    while(m <= h):
+k = 4
 
-        if a[m] == 0:
-            a[m] , a[l] = a[l] , a[m]
-            l += 1
-            m += 1
-        elif a[m] == 1:
-            m += 1
-        else:
-            a[m] , a[h] = a[h] , a[m]
-            h -= 1
-
-    return a
-
-a = [1,2,0,1,0,2,0,1]
-n = len(a)
-print(sort0_1(a,n))
+print(a[k-1])
+print(a[-k])
