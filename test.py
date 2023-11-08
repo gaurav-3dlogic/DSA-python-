eea83175d859ff0d617bf9740c038795e841e676
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-11-08 11:25:18 +0530

    demo

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
-def movenNegitiveOneSide(a):
-    j = 0
-    n = len(a)
+def sort0_1(a,n):
 
-    for i in range(n):
-        if a[i] < 0:
-            a[i] , a[j] = a[j], a[i]
-            j += 1
+    n = len(a)
+    l , m , h = 0 , 0 , n - 1
+    while(m <= h):
+
+        if a[m] == 0:
+            a[m] , a[l] = a[l] , a[m]
+            l += 1
+            m += 1
+        elif a[m] == 1:
+            m += 1
+        else:
+            a[m] , a[h] = a[h] , a[m]
+            h -= 1
+
     return a
 
-a = [1,2,3,-1,0,-9,-2,-4,8,4]
-
-print(movenNegitiveOneSide(a))
+a = [1,2,0,1,0,2,0,1]
+n = len(a)
+print(sort0_1(a,n))
