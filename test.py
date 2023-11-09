9c3daeee5ecf58d16d7ee13c9c38fed32c0c6d09
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-11-09 23:06:12 +0530

    demo

--- test.py
+++ test.py
@@ -1,19 +1,22 @@
+# a = [1,2,3,4,5,6,7,8,9]
+# b = [2,3,4,4,6,8,1]
+
+
+# c = []
+# for i in a:
+#     if i in b and i not in c:
+#         c.append(i)
+# print(c)
 
 
 
-def moveneg(a):
-    n = len(a)
-    j = 0
 
 
-
-    for i in range(n):
-        if a[i] < 0:
-            a[i] , a[j] = a[j], a[i]
-            j += 1
-    return a
-
-a = [1,-2,0,8,-1,-2,9,2,4,5]
-
-
-print(moveneg(a))
+def intersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
+    return lst3
+ 
+# Driver Code
+lst1 = [1,2,3,4,5,6,7,8,9]
+lst2 = [2,3,4,4,6,8,1]
+print(intersection(lst1, lst2))
