1cd3bda7fa882c17209cae1703d8211cb7cbe0f5
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-11-22 22:13:29 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,1 +1,19 @@
 # Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
+
+def sort01(arr):
+    n = len(arr)
+    l,m,h = 0,0, n -1
+    while m <= h:
+        if arr[m] == 0:
+            arr[l] , arr[m] = arr[m] , arr[l]
+            l += 1
+            m += 1
+        elif arr[m] == 1:
+            m += 1
+        else:
+            arr[m] , arr[h] = arr[h] , arr[m]
+            h -= 1
+    return arr
+arr = [2,1,0,1,0,2,1,2,0]
+n = len(arr)
+print(sort01(arr))
