7a3ee4736ea84384089d1e8d2d78b8cbb79fa5d3
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-22 10:00:09 +0530

    demo

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-def reverse(arr):
-    start = 0
-    end = len(arr) -1
+# def reverse(arr):
+#     start = 0
+#     end = len(arr) -1
 
-    while start < end:
-        arr[start], arr[end] = arr[end], arr[start]
-        start += 1
-        end -= 1
+#     while start < end:
+#         arr[start], arr[end] = arr[end], arr[start]
+#         start += 1
+#         end -= 1
 
-    return arr
+#     return arr
 
-arr = [1,2,3,4]
-print(reverse(arr))
+# arr = [1,2,3,4]
+# print(reverse(arr))
     
