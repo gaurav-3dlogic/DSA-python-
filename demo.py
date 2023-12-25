3f73c0b2a4c47354a0105d37b770fcaf262c39c7
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-25 23:32:21 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,17 +1,11 @@
-# def sort01(arr):
-#     n = len(arr)
-#     l,m,h = 0,0, n -1
-#     while m <= h:
-#         if arr[m] == 0:
-#             arr[l] , arr[m] = arr[m] , arr[l]
-#             l += 1
-#             m += 1
-#         elif arr[m] == 1:
-#             m += 1
-#         else:
-#             arr[m] , arr[h] = arr[h] , arr[m]
-#             h -= 1
-#     return arr
-# arr = [2,1,0,1,0,2,1,2,0] 
-# n = len(arr)
-# print(sort01(arr))
+#Reverse a list in python
+def reverseString(s):
+    start , end = 0 , len(s) -1
+    while start <= end:
+        s[start] , s[end] = s[end] , s[start]
+        start += 1
+        end -= 1
+    return s
+s = [1,2,3,4]
+print(reverseString(s))
+
