5c94ba17d45b8165a5ec3ea9d821106a2e28ae07
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-25 23:42:32 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,11 +1,22 @@
-#Reverse a list in python
-def reverseString(s):
-    start , end = 0 , len(s) -1
-    while start <= end:
-        s[start] , s[end] = s[end] , s[start]
-        start += 1
-        end -= 1
-    return s
-s = [1,2,3,4]
-print(reverseString(s))
+#Find the max and min number in array
 
+# a = [4,1,2,5,5]
+# a.sort()
+# print(a[0],a[-1])
+
+
+
+arr = [4,1,2,5,5]
+m = arr[0]
+for i in arr:
+    if i >= m:
+        m = i
+n = arr[0]
+for j in arr:
+    if j <= n:
+        n = j
+print("max",m)
+print("min",n)
+  
+    
+  
