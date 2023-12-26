8c30b226000b9448432a9716dcf5168a362465f6
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-26 23:10:19 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,22 +1,22 @@
-#Find the max and min number in array
+# #Find the max and min number in array
 
-# a = [4,1,2,5,5]
-# a.sort()
-# print(a[0],a[-1])
+# # a = [4,1,2,5,5]
+# # a.sort()
+# # print(a[0],a[-1])
 
 
 
-arr = [4,1,2,5,5]
-m = arr[0]
-for i in arr:
-    if i >= m:
-        m = i
-n = arr[0]
-for j in arr:
-    if j <= n:
-        n = j
-print("max",m)
-print("min",n)
+# arr = [4,1,2,5,5]
+# m = arr[0]
+# for i in arr:
+#     if i >= m:
+#         m = i
+# n = arr[0]
+# for j in arr:
+#     if j <= n:
+#         n = j
+# print("max",m)
+# print("min",n)
   
     
   
