9bc29ece221c51adb92846476ebac2f304431cdc
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-26 23:27:28 +0530

    demo

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
-def reverse(arr):
-    start = 0
-    end = len(arr) -1
+#sort 0,1,2
 
-    while start < end:
-        arr[start], arr[end] = arr[end], arr[start]
-        start += 1
-        end -= 1
 
+def sort0_1_2(arr):
+    low,mid ,high = 0,0,len(arr) -1 
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[mid] , arr[low] = arr[low] , arr[mid]
+            low += 1    
+            mid += 1
+        
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid] , arr[high] = arr[high], arr[mid]
+            high -= 1
+            
     return arr
+arr = [2,0,1,0,2,1,0]   
 
-arr = [1,2,3,4]
-print(reverse(arr))
-    
+print(sort0_1_2(arr))
