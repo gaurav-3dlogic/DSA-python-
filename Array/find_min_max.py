092617873a8dfd8e9b07b23f3fa0f975aab75cdc
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-26 10:12:35 +0530

    demo

--- Array/find_min_max.py
+++ Array/find_min_max.py
@@ -1,24 +1,39 @@
-a = [1,5,2,3,4,5]
-a.sort()
+# a = [1,5,2,3,4,5]
+# a.sort()
 
-print(a[0],a[-1])
+# print(a[0],a[-1])
 
 
 
-a = [1,2,4,5,10,3,6,90,-9,-2,0]
-# after sort -- [-9, -2, 0, 1, 2, 3, 4, 5, 6, 10, 90]
+# a = [1,2,4,5,10,3,6,90,-9,-2,0]
+# # after sort -- [-9, -2, 0, 1, 2, 3, 4, 5, 6, 10, 90]
 
-for i in range(len(a)):
+# for i in range(len(a)):
+#     m = a[0]
+#     for j in a:
+#         if j >= m:
+#             m = j 
+
+
+# for i in range(len(a)):
+#     n = a[0]
+#     for j in a:
+#         if j <= n:
+#             n = j 
+# print("The max number is",m)
+# print("The min number is",n)
+
+
+#Second approach
+def minMax(a):
     m = a[0]
-    for j in a:
-        if j >= m:
-            m = j 
-
-
-for i in range(len(a)):
     n = a[0]
-    for j in a:
-        if j <= n:
-            n = j 
-print("The max number is",m)
-print("The min number is",n)
+    for i in range(len(a)):
+        if a[i] >= m:
+            m = a[i]
+        elif a[i] <= n:
+            n = a[i]
+    return("The Max number is",m,"The Min number is",n) 
+
+a = [1,-2,2,3,10,4]
+print(minMax(a))
