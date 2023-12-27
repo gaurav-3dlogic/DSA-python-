28a7ca0f2bdc1f6be420e564ffc580e861d4cd33
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-27 23:06:06 +0530

    demo

--- test.py
+++ test.py
@@ -1,21 +1,25 @@
+#sort 0 1 2
+
 def sort0_1_2(arr):
-    low, mid , high = 0,0, len(arr) - 1
+    low , mid , high = 0,0,len(arr) - 1
     while mid <= high:
         if arr[mid] == 0:
             arr[mid] , arr[low] = arr[low] , arr[mid]
             mid += 1
             low += 1
-        elif arr[mid] == 1:
-            mid += 1
-        else:
-            arr[mid] , arr[high] = arr[high] , arr[low]
-            high -= 1
         
+        elif arr[mid] == 1:
+            mid += 1 
+
+        else:
+            arr[mid] , arr[high] = arr[high] , arr[mid]
+            high -= 1
 
 
 
     return arr
 
 
-arr = [2,1,0,2,1,0,2]
+arr = [2,0,1,2,0,2,1]
+
 print(sort0_1_2(arr))
