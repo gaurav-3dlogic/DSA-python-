32384ad1566b52b72784ce895c72c60a1d62aa96
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-27 23:44:31 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,18 +1,17 @@
-# Write a program to cyclically rotate an array by one.	
-# def cycle_rotate(arr):
-#     n = len(arr)
-#     last_element = arr[-1]
+#kadane algo 
+def MaxSumSubArray(arr):
 
-#     for i in range(n -1,0,-1):
-#         arr[i] = arr[i -1 ]
+    sum = 0
+    maxSum = 0
 
-#     arr[0] = last_element
+    for i in range(len(arr)):
+        sum += arr[i]
+        maxSum = max(maxSum, sum)
 
+        if sum  < 0:
+            sum = 0
 
-# arr = [1,2,3,4]
-# cycle_rotate(arr)
-# print(arr)
+    return maxSum
 
-#Second Approach
-arr = [1,2,3,4]
-print([arr[-1]] + arr[0:-1])
+arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+print(MaxSumSubArray(arr))
