114138db89a012b72acf27e46bc1d18c32211a89
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-27 09:44:57 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,9 +1,8 @@
-a = [1,3,4,6,3]
-b = [2,3,4,5,3]
+a = [1,-2,3,0,-9,5]
+j = 0
 
-c = []
-
-for i in a:
-    if i in b and i not in c:
-        c.append(i)
-print(c)
+for i in range(len(a)):
+    if a[i] < 0:
+        a[i] , a[j] = a[j], a[i]
+        j += 1
+print(a)
