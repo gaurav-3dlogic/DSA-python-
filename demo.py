17fb8af305dbdf3670d276b4befe117feb225be3
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-27 23:30:37 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,10 +1,18 @@
 # Write a program to cyclically rotate an array by one.	
+# def cycle_rotate(arr):
+#     n = len(arr)
+#     last_element = arr[-1]
+
+#     for i in range(n -1,0,-1):
+#         arr[i] = arr[i -1 ]
+
+#     arr[0] = last_element
+
+
+# arr = [1,2,3,4]
+# cycle_rotate(arr)
+# print(arr)
+
+#Second Approach
 arr = [1,2,3,4]
-
 print([arr[-1]] + arr[0:-1])
-
-
-#output arr = [4,2,3,1]
-
-#Time complexity = 0(n)
-#Space complexity = 0(n)
