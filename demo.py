70745851e289bdb25e2a90309fd3dee6d929e681
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-28 23:36:28 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,10 +1,17 @@
 #Find duplicate in an array of N+1 Integers	
+#First approach
+# a = [1,1,2,3,4,5,5]
+# b = []
+def find_duplicate(arr):
+    res = []
 
-a = [1,1,2,3,4,5,5]
-b = []
 
-for i in range(len(a)):
-    if a.count(i) < 1:
-        b.append(a[i])
-print(b)
-    
+    for i in range(len(arr)):
+        if arr.count(i) > 1:
+            if i not in res:
+                res.append(i)
+    if not res:
+        return -1
+    return res
+arr = [1,1,1,1,2,3,4,5]
+print(find_duplicate(arr))
