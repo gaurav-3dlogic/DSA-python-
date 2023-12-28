cb2cd688bcfb3c2e382947aa3b7839d415bea6a9
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-28 09:55:53 +0530

    demo

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
-def jump(nums):
-    ans = 0
-    end = 0
-    far = 0
+# def jump(nums):
+#     ans = 0
+#     end = 0
+#     far = 0
 
 
-    for i in range(len(nums) -1 ):
-        far = max(far, i + nums[i])
-        if far >= len(nums) -1:
-            ans += 1
-            break 
+#     for i in range(len(nums) -1 ):
+#         far = max(far, i + nums[i])
+#         if far >= len(nums) -1:
+#             ans += 1
+#             break 
 
-        elif i == end:
-            ans += 1
-            end = far
-    return ans
+#         elif i == end:
+#             ans += 1
+#             end = far
+#     return ans
 
-nums = [2,3,1,1,4]
-print(jump(nums))
+# nums = [2,3,1,1,4]
+# print(jump(nums))
