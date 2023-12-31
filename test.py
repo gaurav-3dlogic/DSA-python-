0fc2e05fc1e303d175d0794298f9d0091e2624bd
gaurav-3dloigc <gs3dlogic@gmail.com>
2023-12-31 11:17:33 +0530

    demo

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
-# def mergeTwo(arr1 , a , arr2, b):
-#     i = a - 1
-#     j = b - 1
-#     k =  a + b - 1
+def mergeTwo(arr1 , a , arr2, b):
+    i = a - 1
+    j = b - 1
+    k =  a + b - 1
 
 
-#     while j >= 0:
-#         if i >= 0 and arr1[i] > arr2[j]:
-#             arr1[k] = arr1[i]
-#             i -= 1
-#         else:
-#             arr1[k] = arr2[j]   
-#             j -= 1
-#         k -= 1
-# arr1 = [1,2,3]
-# arr2 = [4,5,6]
-# a = 3
-# b = 3
-# mergeTwo(arr1,a,arr2,b)
-# print(arr1)
+    while j >= 0:
+        if i >= 0 and arr1[i] > arr2[j]:
+            arr1[k] = arr1[i]
+            i -= 1
+        else:
+            arr1[k] = arr2[j]   
+            j -= 1
+        k -= 1
+arr1 = [1,2,3]
+arr2 = [4,5,6]
+a = 3
+b = 3
+mergeTwo(arr1,a,arr2,b)
+print(arr1)
  
