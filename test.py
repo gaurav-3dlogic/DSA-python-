5a34f29c2551ba0f871b2b626e09a90efdcb35b6
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-02 09:53:43 +0530

    demo

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
-#Kadane alogrithm
+# #Kadane alogrithm
 
-def kadane(arr):
-    sum = 0
-    maxSum = 0
-    for i in range(len(arr)):
-        sum += arr[i]
-        maxSum = max(sum, maxSum)
+# def kadane(arr):
+#     sum = 0
+#     maxSum = 0
+#     for i in range(len(arr)):
+#         sum += arr[i]
+#         maxSum = max(sum, maxSum)
 
-        if sum < 0:
-            sum = 0
+#         if sum < 0:
+#             sum = 0
 
 
 
-    return maxSum
+#     return maxSum
 
-arr = [1,2,5,-4,6,1]
-print(kadane(arr))
+# arr = [1,2,5,-4,6,1]
+# print(kadane(arr))
