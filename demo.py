58086ab27acd48bb9c58ca0ed754c66821979523
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-09 23:27:50 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,17 +1,24 @@
-#cyclic rotate array
+# Merge two sorted without using any extra space 
 
+def mergeTwoSorted(arr1,m,arr2,n):
+    i = m -1
+    j = n -1 
+    k = m + n - 1
 
-def cyclic_rotate(arr):
-    n = len(arr)
-    last_element = arr[-1]
+    while j >= 0:
+        if i >= 0 and arr1[i] > arr2[j]:
+            arr1[k] = arr1[i]
+            i -= 1
+        else:
+            arr1[k] = arr2[j]
+            j -= 1
+        k -= 1
+arr1 = [1,2,3,4]
+m = 4
+arr2 = [5,6,7]
+n = 3
 
+mergeTwoSorted(arr1,m,arr2,n)
+print(arr1)
+        
 
-    for i in range(n-1,0,-1):
-        arr[i] = arr[i -1]
-
-    arr[0] = last_element
-
-
-arr = [1,2,3,4]
-cyclic_rotate(arr)
-print(arr)
--- test.py
+++ test.py
@@ -1,38 +1,24 @@
-# def mergeTwoSorted(nums1,m,nums2,n):
-#     i = m -1
-#     j = n -1
-#     k = n + m - 1
+def mergeTwoSorted(nums1,m,nums2,n):
+    i = m -1
+    j = n -1
+    k = n + m - 1
 
-#     while j >= 0:
-#         if i >= 0 and nums1[i] > nums2[j]:
-#             nums1[k] = nums1[i]
-#             i -= 1
-#         else:
-#             nums1[k] = nums2[j]
-#             j -= 1
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
 
-#         k -=1 
+        k -=1 
 
-# nums1 = [1,2,3,4]
-# m = 4
-# nums2 = [4,5,6,7]
-# n = 4
-# mergeTwoSorted(nums1, m,nums2,n)
-# print(nums1)
-
-
-#cyclic rotate
-def cyclic_rotate(arr):
-    n = len(arr)
-
-    last_element = arr[-1]
-
-    for i in range(n-1,0,-1):
-        arr[i] = arr[i - 1] 
-    
-    arr[0] = last_element
+nums1 = [1,2,3,4]
+m = 4
+nums2 = [4,5,6,7]
+n = 4       
+mergeTwoSorted(nums1, m,nums2,n)
+print(nums1)
 
 
-arr = [1,2,3,4,5]
-cyclic_rotate(arr)
-print(arr)
+
