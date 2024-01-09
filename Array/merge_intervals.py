ab2e573fdc8d3e2191d8e15f38ce811be273752d
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-09 23:12:31 +0530

    demo

--- Array/merge_intervals.py
+++ Array/merge_intervals.py
@@ -1,23 +1,23 @@
-# def merge(intervals):
+def merge(intervals):
         
-#         x=[]
-#         if len(intervals)==0:
-#             return x
-#         intervals.sort()
+        x=[]
+        if len(intervals)==0:
+            return x
+        intervals.sort()
         
-#         temp=intervals[0]
-#         for i in intervals:
+        temp=intervals[0]
+        for i in intervals:
             
-#             if temp[1]>=i[0]:
-#                 temp[1]=max(temp[1],i[1])
+            if temp[1]>=i[0]:
+                temp[1]=max(temp[1],i[1])
             
-#             else:
-#                 x.append(temp)
-#                 temp=i
+            else:
+                x.append(temp)
+                temp=i
                 
-#         x.append(temp)     
+        x.append(temp)     
          
-#         return x
+        return x
     
-# intervals = [[1,3],[2,6],[8,10],[15,18]]
-# print(merge(intervals))
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+print(merge(intervals))
--- test.py
+++ test.py
@@ -19,3 +19,20 @@
 # n = 4
 # mergeTwoSorted(nums1, m,nums2,n)
 # print(nums1)
+
+
+#cyclic rotate
+def cyclic_rotate(arr):
+    n = len(arr)
+
+    last_element = arr[-1]
+
+    for i in range(n-1,0,-1):
+        arr[i] = arr[i - 1] 
+    
+    arr[0] = last_element
+
+
+arr = [1,2,3,4,5]
+cyclic_rotate(arr)
+print(arr)
