dc04db3b508a202ef5c7131db5589a9c5fb1e4d0
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-10 11:11:24 +0530

    demo

--- mergeTwoSorted.py
+++ mergeTwoSorted.py
@@ -1,3 +1,5 @@
+#First Appraoch
+
 def merge(nums1, m, nums2, n):
     i = m - 1
     j = n - 1
@@ -21,3 +23,43 @@ merge(nums1, m, nums2, n)
 print(nums1)
 # Time Complexity: O(m + n)
 # Space Complexity: O(1)
+
+
+#Second Approach
+def merge(arr1, arr2, arr3):
+    i = j = k = 0
+    n = len(arr1)
+    m = len(arr2)
+
+    while i < n and j < m:
+        if arr1[i] < arr2[j]:
+            arr3[k] = arr1[i]
+            i += 1
+        else:
+            arr3[k] = arr2[j]
+            j += 1
+        k += 1
+
+    # Copy the remaining elements of arr1
+    while i < n:
+        arr3[k] = arr1[i]
+        i += 1
+        k += 1
+
+    # Copy the remaining elements of arr2
+    while j < m:
+        arr3[k] = arr2[j]
+        j += 1
+        k += 1
+
+def print_array(ans):
+    print(" ".join(map(str, ans)))
+
+# Example usage
+arr1 = [1, 3, 5, 7, 9]
+arr2 = [2, 4, 6]
+arr3 = [0] * (len(arr1) + len(arr2))
+
+merge(arr1, arr2, arr3)
+print_array(arr3)
+
--- test.py
+++ test.py
@@ -1,32 +1,18 @@
-def next_permutation(s):
-    # Step 1: Find the rightmost character that is smaller than the character to its right
-    i = len(s) - 2
-    while i >= 0 and s[i] >= s[i + 1]:
-        i -= 1
+def MergeSortedArrays(arr1, arr2):
+    while (bool(arr1) or bool(arr2)):
+        if(not bool(arr1)):
+            for i in range(len(arr2)):
+                print(arr2[i])
+            arr2 = []
+        elif(not bool(arr2)):
+            for i in range(len(arr1)):
+                print(arr1[i])
+            arr1 = []
+        elif(arr1[0] <= arr2[0]):
+            print(arr1[0])
+            arr1.pop(0)
+        else:
+            print(arr2[0])
+            arr2.pop(0)
 
-    # If no such character is found, the string is already the last permutation
-    if i == -1:
-        return None
-
-    # Step 2: Find the smallest character to the right of i that is greater than s[i]
-    j = len(s) - 1
-    while s[j] <= s[i]:
-        j -= 1
-
-    # Step 3: Swap s[i] and s[j]
-    s_list = list(s)
-    s_list[i], s_list[j] = s_list[j], s_list[i]
-
-    # Step 4: Reverse the substring to the right of i
-    s_list[i + 1:] = reversed(s_list[i + 1:])
-
-    return ''.join(s_list)
-
-# Example usage:
-input_str = "acb"
-next_permuted_str = next_permutation(input_str)
-
-if next_permuted_str:
-    print("Next lexicographically permutation:", next_permuted_str)
-else:
-    print("No next permutation, it's already the last one.")
+MergeSortedArrays([0,1,2,3,0],[5,6,7,8,9])
