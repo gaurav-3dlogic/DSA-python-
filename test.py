9f7c6507028b4ca7db06ef61f7bc48b140bbe1e8
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-11 23:38:14 +0530

    demo

---
+++ test.py
@@ -0,0 +1,26 @@
+#Merge intervals
+
+def merge_intervals(intervals):
+
+    x = []
+    if len(intervals):
+        return x
+    intervals.sort()
+
+
+    temp = intervals[0]
+    for i in intervals:
+
+        if temp[1] >= i[0]:
+            temp[1] = max(temp[1],i[1])
+        else:
+            x.append(temp)
+            temp = i
+
+    x.append(temp)
+    return x 
+
+
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+print(merge_intervals(intervals))
+    
