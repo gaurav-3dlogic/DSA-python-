235e8aac30311c74e5fecbd7d1573625924d439f
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-16 16:20:54 +0530

    demo

--- patterns/patterns1.py
+++ patterns/patterns1.py
@@ -1,4 +1,4 @@
-# for i in range(1,5):
-#     for j in range(1,5):
-#         print(j,end = "")
-#     print()
+for i in range(1,5):
+    for j in range(1,5):
+        print(j,end = "")
+    print()
