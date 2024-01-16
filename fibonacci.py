5a9814ce0c523577174e81adcd7be44ea957d68a
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-16 23:45:33 +0530

    demo

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,31 @@
 # 0 1 1 2 3 5 8 13 21 34
 
-n = int(input("Enter a number: "))
+#Fisrt Approach
+# n = int(input("Enter a number: "))
 
 
-def fibo(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibo(n -1) + fibo(n -2) 
+# def fibo(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibo(n -1) + fibo(n -2) 
 
-print(fibo(n))
+# print(fibo(n))
+
+
+#Second Apporach using generator
+
+def fibonacci(n):
+    # Initialize the first two Fibonacci numbers
+    a, b = 0, 1
+    # Generate Fibonacci numbers up to n
+    while a <= n:
+        yield a
+        a, b = b, a + b
+
+# Generate the Fibonacci series up to 100
+for num in fibonacci(10):
+    print(num)
+
--- test.py
+++ test.py
@@ -1,16 +1,7 @@
-f = int(input("Enter a number: "))
+def print_num(n):
+    for i in range(n):
+        yield i
+print(print(5))
 
-
-def gen(f):
-    for i in range(1,f+1):
-        fact = 1
-
-
-        for j in range(1, i + 1):
-            fact = fact * j 
-        yield fact 
-
-for fact in gen(f):
-    print(fact)
 
 
