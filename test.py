14b66ff6ccba39f992a38524d313d004fb2a733d
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-18 10:03:40 +0530

    demo

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 
-a = [5,9,1,5,10,3]
+# a = [5,9,1,5,10,3]
 
 
-m = a[0]
+# m = a[0]
 
-for i in range(len(a)):
-    if i >= m:
-        m = i
-print(m)
+# for i in range(len(a)):
+#     if i >= m:
+#         m = i
+# print(m)
     
     
 
