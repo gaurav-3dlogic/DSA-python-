1a69833b22ca5d1b4571e6867ec521f8670b1212
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-18 23:55:12 +0530

    demo

---
+++ filter.py
@@ -0,0 +1,14 @@
+a = [1,5,2,3,4,10,12,46]
+
+#First approach
+def even(a):
+    if a % 2 == 0:
+        return a
+        
+res = list(filter(even,a))
+
+print(res)
+
+#Second approach
+res1 = list(filter(lambda a:a % 2 == 0,a))
+print(res1)
