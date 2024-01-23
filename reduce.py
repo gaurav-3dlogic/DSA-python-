3b7c5d8e41010d3bd1306b78aac8107395d2ff88
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-23 10:50:58 +0530

    demo

--- reduce.py
+++ reduce.py
@@ -1,12 +1,12 @@
-from functools import reduce
+# from functools import reduce
 
-a = [1,5,2,3,4,10,12,46]
+# a = [1,5,2,3,4,10,12,46]
 
-def large(a,b):
-    if a > b:
-        return a 
-    else:
-        return b
+# def large(a,b):
+#     if a > b:
+#         return a 
+#     else:
+#         return b
         
-r = reduce(large,a)
-print(r)
+# r = reduce(large,a)
+# print(r)
