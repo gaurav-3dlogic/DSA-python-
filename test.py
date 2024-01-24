48ffff0553b9d03ddeaf412c1d5b99203839a46d
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-24 09:47:32 +0530

    demo

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-rs = [1,2,4,5,67,1,2]
+# rs = [1,2,4,5,67,1,2]
 
-duplicate = []
+# duplicate = []
 
 
-for i in rs:
-    if rs.count(i) > 1:
-        if i not in duplicate:
-            duplicate.append(i)
-print(duplicate)
+# for i in rs:
+#     if rs.count(i) > 1:
+#         if i not in duplicate:
+#             duplicate.append(i)
+# print(duplicate)
