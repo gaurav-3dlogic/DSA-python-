cfd681b4d8dcbcc764a58bc0a33c94f1a84802b5
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-24 23:37:13 +0530

    demo

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-# rs = [1,2,4,5,67,1,2]
+#possible substring
 
-# duplicate = []
+a = "abc"
+res = []
 
 
-# for i in rs:
-#     if rs.count(i) > 1:
-#         if i not in duplicate:
-#             duplicate.append(i)
-# print(duplicate)
+for i in range(len(a)):
+    for j in range(i+1,len(a)+1):
+        res.append(a[i:j])
+
+print(','.join(res))
