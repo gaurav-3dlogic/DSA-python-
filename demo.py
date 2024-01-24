00713ad8e47ef982223102aecf34b9bd7e7619b2
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-24 09:52:25 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,14 +1,14 @@
 
-list1 = [4,1,5,2,4,5]
+# list1 = [4,1,5,2,4,5]
 
-duplicates = []
+# duplicates = []
 
 
-for item in list1:
-    if list1.count(item) > 1:
-        if item not in duplicates:
-            duplicates.append(item)
-print(duplicates)
+# for item in list1:
+#     if list1.count(item) > 1:
+#         if item not in duplicates:
+#             duplicates.append(item)
+# print(duplicates)
 
 
 
