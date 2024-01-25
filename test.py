f7db9e1f0464f55322475d9a45780662059ae08a
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-25 23:37:26 +0530

    demo

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-list1 = [1,2,3,4,5,6,7,8,9,10,11,12,1,2]
-
-for i in list1:
-    if list1.count(i) > 1:
-        list1.remove(i)
-print(list1)
+n = 6
+a = []
 
 
+while(n > 0):
+    dig = n % 2
+    a.append(dig)
+    n = n // 2
+
+
+a.reverse()
+for i in a:
+    print(i, end= "")
