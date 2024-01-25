15eab068e9a87ca4e952d72b5343c2275fbc8a81
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-25 09:46:26 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
-res = [1,2,3,4,5,6,7,8,9,1,2]
+# res = [1,2,3,4,5,6,7,8,9,1,2]
 
 
-for item in res:
-    if res.count(item) > 1:
-        res.remove(item)
-print(res)
+# for item in res:
+#     if res.count(item) > 1:
+#         res.remove(item)
+# print(res)
