3040ae8d3b92b2828542382e9a554613006afd2c
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-25 23:08:50 +0530

    demo

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-from functools import reduce
+# from functools import reduce
 
-res = [1,2,3,4,5,6,7,8,9,10]  
+# res = [1,2,3,4,5,6,7,8,9,10]  
 
-def Sumi(a,b):
-    return a + b
+# def Sumi(a,b):
+#     return a + b
 
 
-print(reduce(Sumi,res))
+# print(reduce(Sumi,res))
+
+
+#second approach
+a = [1,2,3,4]
+
+res = 0
+
+for i in a:
+    res += i
+print(res)
 
 
 
