25ed63a598b7c6734584a489626f12129a278d8b
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-29 23:38:50 +0530

    demo

--- test.py
+++ test.py
@@ -1,22 +1,15 @@
-n = 6
-a = []
+n = 7
+res = []
 
 
-while(n > 0):
+
+
+while n > 0:
     dig = n % 2
-    a.append(dig)
+    res.append(dig)
     n = n // 2
 
+res.reverse()
 
-a.reverse()
-for i in a:
-    print(i, end= "")
-
-
-list1 = [3,1,2,3,5]
-
-
-for item in list1:
-    if list1.count(item) > 1:
-        list1.remove(item)
-print(list1)
+for i in res:
+    print(i,end = "")
