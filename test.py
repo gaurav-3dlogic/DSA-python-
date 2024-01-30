7642131e9bc757fa1a3d0d1c061c171db51498a6
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-30 11:10:29 +0530

    demo

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
+# a = "Hey Gaurav what your problematic"
+
+# b = a.split()
+
+# longest_str = ""
+
+
+# for i in b:
+#     if len(i) > len(longest_str):
+#         longest_str = i
+# print(longest_str)
+
+
+# # Time complexity: O(n)
+# # Space complexity: O(1)
+
+
+
 a = "Hey Gaurav what your problematic"
 
 b = a.split()
