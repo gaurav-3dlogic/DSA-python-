082afaf7b390ac6b3fabc332440c2b4be17b38f6
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-30 22:40:58 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,13 +1,25 @@
-#longest string
 
-b = "hey gaurav how your love is not pythonww"
+b = [2,3,4,5,6,8,9,10]
+n = 5
 
-a = b.split()
 
-res = ""
 
-for i in a:
-    if len(i) > len(res):    
-        res = i
+# Nearest lower number
+lower_number = 0
+for i in b:
+    if i < n:
+        lower_number = i
 
-print(res)
+# Nearest upper number
+upper_number = 0
+for j in b:
+    if j > n:
+        upper_number = j
+        break
+
+print("The nearest lower number is", lower_number)
+print("The nearest upper number is", upper_number)
+
+
+# Time complexity: O(n)
+# Space complexity: O(1)
