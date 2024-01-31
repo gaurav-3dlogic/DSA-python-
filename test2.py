617b93405b6bca91a4892997ccc78306ba7085fc
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-01-31 22:50:46 +0530

    demo

--- test2.py
+++ test2.py
@@ -1,15 +1,39 @@
 #Fibonacci series
 
 
-n = int(input("Enter a number: "))
+# n = int(input("Enter a number: "))
 
-a = 0
-b = 1
-z = 0
+# a = 0
+# b = 1
+# z = 0
 
-while(n >= z):
-    print(z)
-    z = a + b
-    a = b
-    b = z
+# while(n >= z):
+#     print(z)
+#     z = a + b
+#     a = b
+#     b = z
+
+
+# def fibo(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibo(n-1) + fibo(n-2)
+
+# print(fibo(10))
+
+
+def fibonacci(n):
+    a = 0
+    b = 1
+    while n >= a:
+        yield a
+        a, b = b, a + b
+
+
+for i in fibonacci(100):
+    print(i)
+
 
