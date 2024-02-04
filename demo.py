8869a544a56dc4c318542ca41d8afcabcf034f43
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-04 22:27:32 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,14 +1,14 @@
-# a = "He who resposable who creadit the here who ans who"
-# b = "who"
+a = "He who resposable who creadit the here who ans who"
+b = "who"
 
-# pos = []
-# count = 0
+pos = []
+count = 0
 
 
-# for i in range(len(a)): 
-#     if a[i:i + len(b)] == b:
-#         pos.append(i)
-#         count += 1
+for i in range(len(a)): 
+    if a[i:i + len(b)] == b:
+        pos.append(i)
+        count += 1
 
-# print(pos)
-# print(count)
+print(pos)
+print(count)
