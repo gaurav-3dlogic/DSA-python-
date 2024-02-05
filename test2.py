eff78bb83a617449098c87a8f62f95a21c7403a9
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-05 22:59:55 +0530

    demo

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
-num = input("Enter the number: ")
+# num = input("Enter the number: ")
 
 
-for i in range(len(num)):
-    if num[i] not in "01":
-        print("No its not a Binary")
-        break
-else:
-    print("Number is binary")
+# for i in range(len(num)):
+#     if num[i] not in "01":
+#         print("No its not a Binary")
+#         break
+# else:
+#     print("Number is binary")
