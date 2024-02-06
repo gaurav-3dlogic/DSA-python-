9c314d020754e0932a1ba3facfa5c42f122ad750
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-06 23:01:52 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,17 +1,19 @@
-# Make a GET request to this endpoint ”https://jsonplaceholder.typicode.com/posts”. And print all values for the key “title”.
-import requests
+def least_common_multiple(a,b):
+    if a > b:
+        greater = a
+    elif b > a:
+        greater = b
+    while(True):
+        if ((greater % a == 0)) and (greater % b == 0):
+            lcm = greater
+            break
+        greater = greater + 1
+    return lcm
 
-# Make the GET request
-response = requests.get("https://jsonplaceholder.typicode.com/posts")
+a = 10
+b = 20
+print(least_common_multiple(a,b))
+            
+        
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Get the JSON response data
-    data = response.json()
 
-    # Extract and print the values for the key "title"
-    for item in data:
-        title = item["title"]
-        print(title)
-else:
-    print("Error:", response.status_code)
