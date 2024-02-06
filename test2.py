f94f3e680b15ff6623ae4fd7563a7aba27bb12a7
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-06 23:45:05 +0530

    demo

--- test2.py
+++ test2.py
@@ -1,3 +1,12 @@
-a = ['a', 'b', 'c', 'd', 'e', 'e']
-res = list(dict.fromkeys(a))
-print(res)
+a = [1,2,3,4,5,6,7,8,9]
+
+# a[0] ,a[-1] = a[-1],a[0]
+
+# print(a)
+
+temp = 0
+temp = a[0]
+a[0] = a[-1]
+a[-1] = temp
+
+print(a)
