49236253a7802895ec9048bad882ea14deb6b9de
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-07 22:50:32 +0530

    demo

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
-Height=float(input("Enter your height in centimeters: "))
-Weight=float(input("Enter your Weight in Kg: "))
-Height = Height/100
-BMI=Weight/(Height*Height)
-print("your Body Mass Index is: ",BMI)
-if(BMI>0):
-	if(BMI<=16):
-		print("you are severely underweight")
-	elif(BMI<=18.5):
-		print("you are underweight")
-	elif(BMI<=25):
-		print("you are Healthy")
-	elif(BMI<=30):
-		print("you are overweight")
-	else: print("you are severely overweight")
-else:("enter valid details")
+def countString(s):
+    d = {}
+    for i in s:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    return d
+
+s = "printgame"
+print(countString(s))
