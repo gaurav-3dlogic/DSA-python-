adec71a51568b4a3ac5e92dea7ead0c964805bad
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-09 10:29:06 +0530

    demo

--- test2.py
+++ test2.py
@@ -1,10 +1,10 @@
-d = {
-    'ravi': 10,"sorca":8,"sherca":90,"jeo":80
-}
+# d = {
+#     'ravi': 10,"sorca":8,"sherca":90,"jeo":80
+# }
 
-myKeys = list(d.keys())
-myKeys.sort()
-sorted_dict = {i: d[i] for i in myKeys}
+# myKeys = list(d.keys())
+# myKeys.sort()
+# sorted_dict = {i: d[i] for i in myKeys}
 
 
-print(sorted_dict)
+# print(sorted_dict)
