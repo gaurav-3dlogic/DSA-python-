247f149af5874f6c608083ee37a0fbefc890fe94
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-15 23:45:18 +0530

    demo

--- Pair_of_sum_and_count_equal_to_num.py
+++ Pair_of_sum_and_count_equal_to_num.py
@@ -1,27 +1,27 @@
-# # def findPair(arr,target):
-# #     num = {}
-# #     pairs = []
+def findPair(arr,target):
+    num = {}
+    pairs = []
     
 
     
-# #     for i in arr:
-# #         c = target - i
-# #         if c in num:
-# #             pair = (i,c)
-# #             pairs.append(pair)
+    for i in arr:
+        c = target - i
+        if c in num:
+            pair = (i,c)
+            pairs.append(pair)
             
-# #         if i in num:
-# #             num[i] += 1
-# #         else:
-# #             num[i] = 1
-# #     return pairs
-# # arr = [1, 5, 7, -1]
-# # target = 6
+        if i in num:
+            num[i] += 1
+        else:
+            num[i] = 1
+    return pairs
+arr = [1, 5, 7, -1]
+target = 6
 
-# # print(findPair(arr,target))
+print(findPair(arr,target))
 
 
-# #count pairs
+#count pairs
 # def count_pairs_with_sum(arr, target):
 #     num = {}  
 #     count = 0   
