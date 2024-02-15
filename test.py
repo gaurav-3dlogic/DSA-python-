a4094a065b649cc7155fe0f300ae6857ece19556
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-15 23:16:44 +0530

    demo

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-#find the index of list
+a = [2,3,4,1,6,7,8]
+n = 5
 
-a = [5,1,2,3,7]
+
+def pairSum(a,n):
+    d = {}
 
 
-for a , i in enumerate(a):
-    print(a,i)
+    for i in range(len(a)):
+        p = n - a[i]
+        if p in d:
+            return [p,a[i]]
+        else:
+            d[a[i]] = i
+print(pairSum(a,n))
+
