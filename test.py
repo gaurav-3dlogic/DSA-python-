2b0e0a64b0406c6c16c1f0839878a43628cb05cf
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-16 23:39:47 +0530

    demo

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-a = [2,3,4,1,6,7,8]
-n = 5
+# a = [2,3,4,1,6,7,8]
+# n = 5
 
 
-def pairSum(a,n):
-    d = {}
+# def pairSum(a,n):
+#     d = {}
 
 
-    for i in range(len(a)):
-        p = n - a[i]
-        if p in d:
-            return [p,a[i]]
-        else:
-            d[a[i]] = i
-print(pairSum(a,n))
+#     for i in range(len(a)):
+#         p = n - a[i]
+#         if p in d:
+#             return [p,a[i]]
+#         else:
+#             d[a[i]] = i
+# print(pairSum(a,n))
 
