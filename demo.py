883e3509372d314f41f0a93f48b8a5f8bc93ce8e
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-16 23:47:26 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,11 +1,6 @@
-a = [0,10,11,12,13,1,2,4,5,6,7,8,9]
-
-n = 4
+a = [1,3,13,12,2,7,8,10,2]
+n = 2
 
 a.sort()
-
-min = a[n-1]
-max = a[-n]
-
-print("The maximum value",max)
-print("The minimum value",min)
+print(a[n-1])
+print(a[-n])
