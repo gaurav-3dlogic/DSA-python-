92e38c399733f262caeeb5431a62891bcd5e2f86
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-16 23:45:02 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
-def wordCOunt(n,str):
-    a = str.split(' ')
-    res = []
-    for i in a:
-        if len(i) > n:
-            res.append(i)
-    return res
-print(wordCOunt(3,"Hey Bro what happened"))
+a = [0,10,11,12,13,1,2,4,5,6,7,8,9]
+
+n = 4
+
+a.sort()
+
+min = a[n-1]
+max = a[-n]
+
+print("The maximum value",max)
+print("The minimum value",min)
