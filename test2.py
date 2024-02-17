e24b070884c1d2b690b1a9cb00a67ea61db03aa5
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-17 23:20:34 +0530

    demo

--- test2.py
+++ test2.py
@@ -1,17 +1,17 @@
-#Sort01
+# #Sort01 by using loop
 
-def sort01(arr):
-    l,m,h = 0,0 , len(arr) -1
-    while m <= h:
-        if arr[m] == 1:
-            arr[m] , arr[l] = arr[l] , arr[m]
-            l += 1
-            m += 1
-        elif arr[m] == 0:
-            m += 1
-        else:
-            arr[h] , arr[m] = arr[m] , arr[h]
-            h -= 1
-    return arr
-arr = [1,2,0,0,1,2]
-print(sort01(arr))
+# def sort01(arr):
+#     l,m,h = 0,0 , len(arr) -1
+#     while m <= h:
+#         if arr[m] == 0:
+#             arr[m] , arr[l] = arr[l] , arr[m]
+#             l += 1
+#             m += 1
+#         elif arr[m] == 1:
+#             m += 1
+#         else:
+#             arr[h] , arr[m] = arr[m] , arr[h]
+#             h -= 1
+#     return arr
+# arr = [1,2,0,0,1,2]
+# print(sort01(arr))
