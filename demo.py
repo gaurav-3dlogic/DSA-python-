bad0731250652fac2b59be811dd9c94291257e30
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-19 22:52:32 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,6 +1,21 @@
-a = [1,3,13,12,2,7,8,10,2]
-n = 2
+#Sort01
 
-a.sort()
-print(a[n-1])
-print(a[-n])
+
+def sort01(arr):
+    low,mid, high = 0,0,len(arr) - 1
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low] , arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[high] , arr[mid] = arr[mid], arr[ high]
+            high -= 1
+    return arr
+arr = [2,0,1,1,2,0]
+
+print(sort01(arr))
+
+        
