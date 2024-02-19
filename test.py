f26efc5efada08e534c2b68e3481383bc22402c3
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-19 23:03:13 +0530

    demo

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
-#Sort01
+# #Sort01
 
 
-def sort012(arr):
-    l,m,h = 0,0, len(arr) -1 
-    while(m <= h):
-        if arr[m] == 0:
-            arr[l] , arr[m] = arr[m] , arr[l]
-            l += 1
-            m += 1
-        elif arr[m] == 1:
-            m += 1
-        else:
-            arr[m] , arr[h] = arr[h] , arr[m]
-            h -= 1
-    return arr
+# def sort012(arr):
+#     l,m,h = 0,0, len(arr) -1 
+#     while(m <= h):
+#         if arr[m] == 0:
+#             arr[l] , arr[m] = arr[m] , arr[l]
+#             l += 1
+#             m += 1
+#         elif arr[m] == 1:
+#             m += 1
+#         else:
+#             arr[m] , arr[h] = arr[h] , arr[m]
+#             h -= 1
+#     return arr
 
-arr = [2,1,0,0,1,2]
-print(sort012(arr))
+# arr = [2,1,0,0,1,2]
+# print(sort012(arr))
