fbc8ee75402a29ce5f370c3b7b454634eb962264
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-19 23:29:50 +0530

    demo

--- test2.py
+++ test2.py
@@ -1,17 +1,22 @@
-#Sort01 by using loop
+def binary(arr,target):
+    left,right = 0 , len(arr) - 1
 
-def sort01(arr):
-    l,m,h = 0,0 , len(arr) -1
-    while m <= h:
-        if arr[m] == 0:
-            arr[m] , arr[l] = arr[l] , arr[m]
-            l += 1
-            m += 1
-        elif arr[m] == 1:
-            m += 1
+    while left < right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid 
+        
+        elif arr[mid] > target:
+            right = mid - 1
+        
         else:
-            arr[h] , arr[m] = arr[m] , arr[h]
-            h -= 1
-    return arr
-arr = [1,2,0,0,1,2]
-print(sort01(arr))
+            left = mid + 1
+    return -1
+sorted_elems = [1,2,3,4,5,6,7,8,9,23,24,25,26,27,28]
+target = 8
+result = binary(sorted_elems, target)
+if result != -1:
+    print(f"Element {sorted_elems} found at index {result}.")
+else:
+    print(f"Element {sorted_elems} not found in the list.")
