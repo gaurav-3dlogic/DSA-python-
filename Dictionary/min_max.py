80fc0ee1357ea3a5c44b7a3fbe9b5d61b92d40d1
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-21 23:42:55 +0530

    demo

--- Dictionary/min_max.py
+++ Dictionary/min_max.py
@@ -6,7 +6,7 @@ def max_min_dict(dict1):
             max_val = value
         if value > min_val:
             min_val = value
-    return max_val, min_val
+    return max_val, min_val 
 
 dict1 = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
 print(max_min_dict(dict1))
--- demo.py
+++ demo.py
@@ -1,14 +1,12 @@
-def Revrsedict(dict):
-    maxValue = float("-Inf")
-    minValue = float("Inf")
-    for key, val in dict.items():
-        if val < maxValue:
-            maxValue = val
-        elif val > minValue:
-            minValue = val
-        
-    return maxValue, minValue
-dict = {"a":1, "b":2, "c":3, "d":4}
+def max_min_dict(dict1):
+    max_val = float("-Inf")
+    min_val = float("Inf")
+    for key, value in dict1.items():
+        if value < max_val:
+            max_val = value
+        if value > min_val:
+            min_val = value
+    return max_val, min_val 
 
-print(Revrsedict(dict))        
-
+dict1 = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
+print(max_min_dict(dict1))
