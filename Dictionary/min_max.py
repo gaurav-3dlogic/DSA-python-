743d798c44a3db1e74c7ce7e437062726af25818
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-21 23:45:45 +0530

    demo

--- Dictionary/min_max.py
+++ Dictionary/min_max.py
@@ -2,11 +2,11 @@ def max_min_dict(dict1):
     max_val = float("-Inf")
     min_val = float("Inf")
     for key, value in dict1.items():
-        if value < max_val:
+        if value > max_val:
             max_val = value
-        if value > min_val:
+        if value < min_val:
             min_val = value
-    return max_val, min_val 
+    return max_val, min_val
 
 dict1 = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
 print(max_min_dict(dict1))
