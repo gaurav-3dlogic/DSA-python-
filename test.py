8f881dee5f3c85bbec404729e9d5a0edf45e29fe
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-22 23:44:19 +0530

    demo

--- test.py
+++ test.py
@@ -9,10 +9,10 @@ arr = [1,2,-2,1,0,-10,-9,1,7]
 def MoveAllNeg(arr):
     nonZero = 0
     for i in range(len(arr)):
-        if arr[i] != 0:
+        if arr[i] > 0 :
             arr[nonZero] , arr[i] = arr[i] , arr[nonZero]
             nonZero += 1
-
+    return arr
 
 MoveAllNeg(arr) 
 print(arr)
