27247ce0eed79787a6f1a3739f61ef6f94b67b51
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-22 23:31:46 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,14 +1,16 @@
-def MinMin(dict):
-    maxVal = float("-Inf")
-    minVal = float("Inf")
+arr = [2,1,0,2,1,0,2,1]
 
-    for k, v in dict.items():
-        if v > maxVal:
-            maxVal = v
-        if v < minVal:
-            minVal = v
-    return maxVal, minVal
-
-dict = {"a":1,"b" : 2,"c":3 }
-
-print(MinMin(dict))
+def Sort01(arr):
+    low,mid,high = 0,0,len(arr) - 1
+    while mid < high:
+        if arr[mid] == 0:
+            arr[low],arr[mid] = arr[mid],arr[low]
+            mid += 1
+            low += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid],arr[high] = arr[high],arr[mid]
+            high -= 1
+    return arr
+print(Sort01(arr))
