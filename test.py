9025db6a06cf282adacacfff4a2b3aa908471245
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-23 23:32:25 +0530

    demo

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
-# #Move all the negative elements to one side of the array	
+#Move all the negative elements to one side of the array	
 
-# arr = [1,2,-2,1,0,-10,-9,1,7]
-
-
+arr = [1,2,-2,1,0,-10,-9,1,7]
 
 
 
-# def MoveAllNeg(arr):
-#     nonZero = 0
-#     for i in range(len(arr)):
-#         if arr[i] > 0 :
-#             arr[nonZero] , arr[i] = arr[i] , arr[nonZero]
-#             nonZero += 1
-#     return arr
 
-# MoveAllNeg(arr) 
-# print(arr)
+
+def MoveAllNeg(arr):
+    nonZero = 0
+    for i in range(len(arr)):
+        if arr[i] > 0 :
+            arr[nonZero] , arr[i] = arr[i] , arr[nonZero]
+            nonZero += 1
+    return arr
+
+MoveAllNeg(arr) 
+print(arr)
             
 
 
