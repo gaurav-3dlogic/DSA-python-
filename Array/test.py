82232445b82e11f499a1ec796a466fa2e7513080
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-02-28 09:45:48 +0530

    demo

--- Array/test.py
+++ Array/test.py
@@ -1,6 +1,6 @@
-n = int(input("Enter the Number: "))
-f = 1
+# n = int(input("Enter the Number: "))
+# f = 1
 
-for i in range(1,n+1):
-    f = f * i 
-print(f)
+# for i in range(1,n+1):
+#     f = f * i 
+# print(f)
