d099bbca54149b04865c1d146f13efa3fe6ee3cb
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-12 00:05:01 +0530

    demo

--- test2.py
+++ test2.py
@@ -1,11 +1,17 @@
-a = "abc"
+#Sort 0,1,2
 
-def reverseString(arr):
-    s , e = 0 ,len(arr) - 1
-    arr = list(arr)
-    while s < e:
-        arr[s] , arr[e] = arr[e] ,arr[s]
-        s += 1
-        e -= 1
-    return ''.join(arr)
-print(reverseString(a))
+def sort012(arr):
+    low,mid , high = 0 , 0 , len(arr) -1 
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low] , arr[mid] = arr[mid] , arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[high] , arr[mid] = arr[mid] , arr[high]
+            high -= 1
+    return arr
+arr = [2,1,0,1,0,2,0,1,2,0]
+print(sort012(arr))
