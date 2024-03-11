7997cf41a7b864f70ab867d20bc4b139ef5959f1
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-11 23:32:25 +0530

    demo

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 # Reverse String 
-def reverse_string(arr):
-    start , end = 0, len(arr) -1
-    while start < end:
-        arr[start], arr[end] = arr[end], arr[start]
-        start += 1
-        end -= 1
-    return arr
-arr = [1,2,3]
-print(reverse_string(arr))
+arr = [1,2,3,4,6]
+
+n = arr[0]
+
+m = arr[0]
+for i in arr:
+    if i < m:
+        m = i 
+    else:
+        n = i 
+
+print(m)
+print(n)
