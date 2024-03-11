da28a455e163a918dc9ba194ad7d0b4f5647185b
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-11 09:39:29 +0530

    demo

--- test.py
+++ test.py
@@ -1,24 +1,24 @@
-a = {
-"content":[
-{
-"attachment":{
-"href":"cid:63f5e4909ca9205450a40030",
-"len":4,
-"name":"Checklist_Ericsson_PKC_INM_CGNAT.txt",
-"type":"text/plain",
-"xmime:contentType":"text/plain"
-}
-},
-{
-"attachment":{
-"href":"cid:642d4b2aceab0054508de410",
-"len":10338,
-"name":"dump.csv",
-"type":"application/octet-stream",
-"xmime:contentType":"application/octet-stream"
-}
-}
-]
-}
+# a = {
+# "content":[
+# {
+# "attachment":{
+# "href":"cid:63f5e4909ca9205450a40030",
+# "len":4,
+# "name":"Checklist_Ericsson_PKC_INM_CGNAT.txt",
+# "type":"text/plain",
+# "xmime:contentType":"text/plain"
+# }
+# },
+# {
+# "attachment":{
+# "href":"cid:642d4b2aceab0054508de410",
+# "len":10338,
+# "name":"dump.csv",
+# "type":"application/octet-stream",
+# "xmime:contentType":"application/octet-stream"
+# }
+# }
+# ]
+# }
 
-print(a['content'][0]['attachment']['name'])
+# print(a['content'][0]['attachment']['name'])
