90ad48c593ade0762ebf92cad271b91bfe44a299
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-12 09:43:51 +0530

    demo

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-a = [9,1,10,2,4,5,12]
-a.sort()
-k = 2
+#Move all negitive number one side
+arr = [1,-2,3,-9,10,-3,-5,6,0]
 
-print(a[k -1])
-print(a[-k])
+def moveOneSide(arr):
+
+    j = 0 
+    n = len(arr)
+
+
+    for i in range(n):
+        if arr[i] < 0:
+            arr[i] , arr[j] = arr[j] , arr[i]
+            j += 1
+    return arr
+print(moveOneSide(arr))
