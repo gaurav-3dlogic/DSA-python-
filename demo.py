45836990b23033a0aff57ba3c7a09829bf392547
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-12 22:57:07 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,16 +1,8 @@
-arr = [2,1,0,2,1,0,2,1]
+a = [1,2,4,5,6,6]
+b = [4,5,6,7,8,6]
+c = []
 
-def Sort01(arr):
-    low,mid,high = 0,0,len(arr) - 1
-    while mid < high:
-        if arr[mid] == 0:
-            arr[low],arr[mid] = arr[mid],arr[low]
-            mid += 1
-            low += 1
-        elif arr[mid] == 1:
-            mid += 1
-        else:
-            arr[mid],arr[high] = arr[high],arr[mid]
-            high -= 1
-    return arr
-print(Sort01(arr))
+for i in a:
+    if i in b and i not in c:
+        c.append(i)
+print(c)
