9b6bb0ccd4faf0795ed291b66178fc72d195a7d0
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-12 23:59:48 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
-arr = [1, 2, 4, 5, 6, 6]
+arr = [1, 2, 4, 5, 6]
 
 
 def cyclic(arr):
     n = len(arr)
     last_ele = arr[-1]
-    for i in range(n-1,0,1):
+    for i in range(n-1,0,-1):
         arr[i] = arr[i-1]
     arr[0] = last_ele
 
--- test.py
+++ test.py
@@ -1,15 +1,9 @@
-a = [1,2,3,4,5,6,7,8,9]
-# print([a[-1]] + a[0:-1])
-
-def cyclically_rotate_array(arr):
+arr = [1,2,3,4,5,6]
+def cycle(arr):
+    last_ele = arr[-1]
     n = len(arr)
-
-    last_element = arr[-1]
-
-    for i in range(n - 1, 0,1):
-        arr[i] = arr[i - 1]
-    arr[0] = last_element
-
-arr = [1,2,3,4,5,6,7,8]
-cyclically_rotate_array(arr)
-print(arr)  
+    for i in range(n-1,0,1):
+        arr[i] = arr[i -1]
+    arr[0] = last_ele
+cycle(arr)
+print(arr)
