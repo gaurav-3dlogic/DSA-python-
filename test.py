30908cae88fd93f8a02e609d4c73dc900e5ebea2
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-12 22:42:47 +0530

    demo

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 
-# arr = [1,-2,3,-9,10,-3,-5,6,0]
+arr = [1,-2,3,-9,10,-3,-5,6,0]
 
-# def allElementRights(arr):
-#     n = len(arr)
-#     j = 0
+def moveAllN(arr):
+    n = len(arr)
+    j = 0
 
-#     for i in range(n):
-#         if arr[i] < 0:
-#             arr[j],arr[i] = arr[i],arr[j]
-#             j += 1
-#     return arr
-# print(allElementRights(arr))
+    for i in range(n):
+        if arr[i] <= 0:
+            arr[j],arr[i] = arr[i],arr[j]
+            j += 1
+    return arr
+print(moveAllN(arr))
 
 
