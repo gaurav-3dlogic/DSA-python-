be427b572bb9f767270be8e2dd842eff4abb18a8
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-13 23:45:31 +0530

    demo

---
+++ demo.py
@@ -0,0 +1,16 @@
+arr = [1,-2,0,3,4,5,7]
+
+
+def kadane(arr):
+    sum = 0
+    maxSum = 0
+
+    for i in range(len(arr)):
+        sum += arr[i]
+        maxSum = max(maxSum, sum)
+
+        if sum < 0:
+            sum = 0
+    return maxSum
+print(kadane(arr))
+
