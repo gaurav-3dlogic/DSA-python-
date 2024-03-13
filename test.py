d2091c41b3d1489fa6c2ea9f29d864a16d13ce80
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-13 10:41:04 +0530

    demo

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-arr = [1,2,3,4,5,6]
-def cycle(arr):
+arr = [1,2,3,4,6]
+
+
+def cyclic(arr):
     last_ele = arr[-1]
     n = len(arr)
-    for i in range(n-1,0,1):
-        arr[i] = arr[i -1]
+    for i in range(n-1,0,-1):
+        arr[i] = arr[i - 1]
     arr[0] = last_ele
-cycle(arr)
+cyclic(arr)
 print(arr)
+
