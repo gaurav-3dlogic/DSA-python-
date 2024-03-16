88fc17cc76ea11de4cc79f25a903622dd02d2435
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-16 16:14:56 +0530

    demo

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-# #CYCLIC ROTATE
-# a = [1,2,3,5,6]
+#CYCLIC ROTATE
+a = [1,2,3,5,6]
 
-# def cyclic(arr):
-#     last_ele = arr[-1]
-#     for i in range(len(arr)-1,0,-1):
-#         arr[i] = arr[i - 1]
-#     a[0] = last_ele
+def cyclic(arr):
+    last_ele = arr[-1]
+    for i in range(len(arr)-1,0,-1):
+        arr[i] = arr[i - 1]
+    a[0] = last_ele
 
-#     return arr
-# cyclic(a)
-# print(a)
+    return arr
+cyclic(a)
+print(a)
