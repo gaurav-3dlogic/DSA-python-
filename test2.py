09e82fc6a9f88b89e2a1ed5ababac2d1ae57903d
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-18 00:03:13 +0530

    demo

--- test2.py
+++ test2.py
@@ -1,17 +1,16 @@
-arr = [2,0,1,2,1,2,0]
+a = [2,3,4,5,0,1]
+
+n = 5
 
 
-def sort01(arr):
-    low,mid , high = 0 , 0, len(arr) -1 
-    while mid <= high:
-        if arr[mid] == 0:
-            arr[low] , arr[mid] = arr[mid] , arr[low]
-            mid += 1
-            low += 1
-        elif arr[mid] == 1:
-            mid += 1
+def pairSum(a,n):
+    d = {}
+    
+    
+    for i in range(len(a)):
+        p = n - a[i] 
+        if p in d:
+            return [p,a[i]]
         else:
-            arr[mid] , arr[high] = arr[high] , arr[mid]
-            high -= 1
-    return arr
-print(sort01(arr))
+            d[a[i]] = i 
+print(pairSum(a,n))
