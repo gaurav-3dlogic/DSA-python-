95ec9d27ab0251caf15454b90467deff6c4aa1f3
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-17 23:49:01 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,19 +1,19 @@
 #cyclic rotate
 
-a = [1,2,3,4,5,6]
+# a = [1,2,3,4,5,6]
 
-def cyclic_rotate(a):
+# def cyclic_rotate(a):
 
-    last_ele = a[-1]
+#     last_ele = a[-1]
 
-    for i in range(len(a) - 1,0,-1):
-        a[i] = a[i - 1]
+#     for i in range(len(a) - 1,0,-1):
+#         a[i] = a[i - 1]
 
-    a[0] = last_ele
+#     a[0] = last_ele
 
-a = [1,2,3,4,5,6]
-cyclic_rotate(a)
-print(a)
+# a = [1,2,3,4,5,6]
+# cyclic_rotate(a)
+# print(a)
 
 
 
--- test.py
+++ test.py
@@ -1,12 +1,9 @@
-
-a = [1,2,3,5,6]
-
+arr = [1,2,3,4,5]
 def cyclic(arr):
     last_ele = arr[-1]
     for i in range(len(arr)-1,0,-1):
-        arr[i] = arr[i - 1]
-    a[0] = last_ele
+        arr[i] = arr[i- 1]
+    arr[0] = last_ele
 
-    return arr
-cyclic(a)
-print(a)
+cyclic(arr)
+print(arr)
