36dc2ecd19673a0395eff6d3042db68135b00e6e
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-18 23:40:09 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,16 +1,12 @@
 n = int(input("Enter a number: "))
 
 if n > 1:
-    is_prime = True  # Assume n is prime initially
-
-    for i in range(2, n):
+    for i in range(2,n):
         if n % i == 0:
-            is_prime = False  # If n is divisible by any number in the range, it's not prime
+            print("Number is not prime")
             break
-
-    if is_prime:
-        print("Number is prime")
     else:
-        print("Number is not prime")
+        print("Number is prime")
+
 else:
     print("Number is not prime")
