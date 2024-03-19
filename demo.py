7d199f22960b5134cec026fec966f4346ffc31d7
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-19 23:55:16 +0530

    demo

--- demo.py
+++ demo.py
@@ -1,16 +1,17 @@
 arr = [1,3,4,2,10,-5]
 target = 5
-pairs = []
-d = {}
+
 
 
 
 def pairOfSum(arr,target):
+    pairs = []
+    d = {}
     for i in range(len(arr)):
         p = target - arr[i]
         if p in d:
-            pairs.append(p,[arr[i]])
+            pairs.append([p,arr[i]])
         else:
-            p = arr[i]
+            d[arr[i]] = i 
     return pairs
 print(pairOfSum(arr,target))
