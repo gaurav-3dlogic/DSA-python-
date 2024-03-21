071f683470022b51f328c1779c193df2339ff6b0
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-21 23:25:45 +0530

    demo

--- test.py
+++ test.py
@@ -2,12 +2,29 @@
 
 n = int(input("Enter a number: "))
 
+#FOr loop 
+# def Prime(n):
+#     if n > 1:
+#         for i in range(2,n):
+#             if n % i == 0:
+#                 print("Number is not prime")
+#                 break
+#         else:
+#             print("Number is prime")
+#     else:
+#         print("Number is not prime")
+
+# Prime(n)
+
+#Using While Loop
+
 if n > 1:
-    for i in range(2,n):
-        if n % i == 0:
+    while(2,n):
+        if n % 2 == 0:
             print("Number is not prime")
             break
     else:
         print("Number is prime")
 else:
     print("Number is not prime")
+        
