3ed72bb224ac314b7e81eb857518c084197ec978
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-03-26 09:49:10 +0530

    demo

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 
+#test
+# n = 100
 
-n = 100
-
-for i in range(2,n):
+# for i in range(2,n):
     
-    for j in range(i+1,n):
-        if i % j == 0:
-            break
-    else:
-        print(i)
+#     for j in range(i+1,n):
+#         if i % j == 0:
+#             break
+#     else:
+#         print(i)
       
