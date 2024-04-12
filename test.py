6ff2eccf23288e9b57e9f7415da3be66503464ec
gaurav-3dloigc <gs3dlogic@gmail.com>
2024-04-13 00:07:15 +0530

    demo

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ class gaurav:
     def __init__(self,name,age):
         self.name = name
         self.age = age
+
+    def __str__(self):
+        return f"{self.name},{self.age}"
 p1 = gaurav("Akash",20)
-print(p1.name)
-print(p1.age)
+print(p1)
