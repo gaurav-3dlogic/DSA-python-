1970e736c3ba6b348174f71497700f0054f94527
devcyno <dev05@cyno.co.in>
2024-05-13 23:09:42 +0530

    new update

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 #test new
-# def decor(printer):
-#     def inner():
-#         printer()
-#         print("inner")
-#     return inner
+def decor(printer):
+    def inner():
+        printer()
+        print("inner")
+    return inner
 
-# @decor
-# def printer():
-#     print("test")
+@decor
+def printer():
+    print("test")
 
-# printer()
+printer()
 
 
