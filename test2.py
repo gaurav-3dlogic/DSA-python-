93e540d2f958afc642b388b2634820a7ccde857a
devcyno <dev05@cyno.co.in>
2024-05-17 09:55:29 +0530

    new update

--- test2.py
+++ test2.py
@@ -1,20 +1,16 @@
-a = [12, 10, 23, -9, 2, 1]
+# class Cat:
 
-# Find the range of elements in the list a
-max_val = max(a)
-min_val = min(a)
-range_val = max_val - min_val + 1
+#     def __init__(self):
+#         self.sound = "meow"
+    
+#     def speak(self):
+#         print(f"Cat says: {(self.__sound)}")
 
-# Create a count array to store the frequency of elements
-count = [0] * range_val
 
-# Count the frequency of each element in the list
-for num in a:
-    count[num - min_val] += 1
+# c = Cat()
+# c.speak()
 
-# Reconstruct the sorted list using the count array
-sorted_list = []
-for i in range(range_val):
-    sorted_list.extend([i + min_val] * count[i])
+# c.sound = "meow"
+# c.speak()
 
-print(sorted_list)
+    
